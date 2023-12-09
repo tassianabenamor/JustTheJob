7b91e444de9b5f0680cd860917e367172d52f763
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-08 21:24:11 -0300

    Classe Gerente

---
+++ src/main/resources/Gerente.java
@@ -0,0 +1,13 @@
+public class Gerente {
+    private String nome;
+    private int id;
+
+    // Construtor
+    public Gerente(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    // Getters e Setters
+    // ...
+}
