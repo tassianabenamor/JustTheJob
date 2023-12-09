fbe97a2250271fac770101fde74bdc7554d617d7
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-08 21:28:54 -0300

    Classe Cotacao

---
+++ src/main/resources/Cotacao.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Cotacao {
+    private Date data;
+    private float valor;
+
+    // Construtor
+    public Cotacao(Date data, float valor) {
+        this.data = data;
+        this.valor = valor;
+    }
+
+    // Getters e Setters
+    // ...
+}
