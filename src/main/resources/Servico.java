ea14b4328775166e35c0473cb0d2bb21c40152e3
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-08 21:27:54 -0300

    Classe Servico

---
+++ src/main/resources/Servico.java
@@ -0,0 +1,13 @@
+public class Servico {
+    private String descricao;
+    private float preco;
+
+    // Construtor
+    public Servico(String descricao, float preco) {
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    // Getters e Setters
+    // ...
+}
