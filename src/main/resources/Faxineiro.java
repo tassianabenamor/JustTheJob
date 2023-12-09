3362cf96f8cce7f31915577eaf25a23f7c89f9e3
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-08 21:25:23 -0300

    Classe Faxineiro

---
+++ src/main/resources/Faxineiro.java
@@ -0,0 +1,16 @@
+public class Faxineiro {
+    private String nome;
+    private int id;
+    private int horasTrabalhadas;
+
+    // Construtor
+    public Faxineiro(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+        this.horasTrabalhadas = 0;
+    }
+
+    // Métodos para manipular horas trabalhadas
+    // Getters e Setters
+    // ...
+}
