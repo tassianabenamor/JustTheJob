b6da51cb0dc69fd204d952207a85b04f0573e67c
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-08 21:23:31 -0300

    Classe Recepcionista

---
+++ src/main/resources/Recepcionista.java
@@ -0,0 +1,13 @@
+public class Recepcionista {
+    private String nome;
+    private int id;
+
+    // Construtor
+    public Recepcionista(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    // Getters e Setters
+    // ...
+}
