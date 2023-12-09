8319a9ea907c63e4bdaa94304706a9cbc168e90b
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-08 21:30:10 -0300

    Classe Agenda

---
+++ src/main/resources/Agenda.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Agenda {
+    private Date data;
+    private String atividades;
+
+    // Construtor
+    public Agenda(Date data, String atividades) {
+        this.data = data;
+        this.atividades = atividades;
+    }
+
+    // Getters e Setters
+    // ...
+}
