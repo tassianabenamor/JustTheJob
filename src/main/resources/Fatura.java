65bcf4b42829191d929b6a0f8c18ab8d2649b9d8
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-08 21:29:32 -0300

    Classe Fatura

---
+++ src/main/resources/Fatura.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Fatura {
+    private Date dataEmissao;
+    private float valorTotal;
+
+    // Construtor
+    public Fatura(Date dataEmissao, float valorTotal) {
+        this.dataEmissao = dataEmissao;
+        this.valorTotal = valorTotal;
+    }
+
+    // Getters e Setters
+    // ...
+}
